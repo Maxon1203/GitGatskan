793485e428a51719692ba5bcf63e5a8f72f31dba
Max <isip_m.n.gackan@mpt.ru>
2021-02-02 10:36:21 +0300

    Gatskan

---
+++ GitPract1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitPract1
+{
+    class Class1
+    {
+        public static void Help()
+        {
+            Console.WriteLine("Help");
+        }
+    }
+}
